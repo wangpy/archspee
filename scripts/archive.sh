d1662623c599993ac53a26a5e80985043a10f7d1
Brian Wang <wangpy.tw@gmail.com>
2019-01-21 19:02:39 -0800

    Add exclude dir in archive script

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -2,4 +2,4 @@
 sudo find . -name '*.pyc' -delete
 sudo find . -name '__pycache__' -delete
 rm -f ../archspee.tgz
-tar -pczvlf ../archspee.tgz --exclude=env *
+tar -pczvlf ../archspee.tgz --exclude=env --exclude=.git *
