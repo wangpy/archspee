83f21d1ebb1fa5cbaa3338992d53d786282e9db1
Brian Wang <wangpy.tw@gmail.com>
2019-01-21 19:02:21 -0800

    Improve intent scripts

--- intent_scripts/turn_backlight_off.sh
+++ intent_scripts/turn_backlight_off.sh
@@ -1,1 +1,11 @@
-sudo sh -c "echo 1 > /sys/class/backlight/rpi_backlight/bl_power"
+#!/bin/bash
+FILES=(
+    '/sys/class/backlight/rpi_backlight/bl_power'
+    '/sys/class/backlight/soc:backlight/bl_power'
+)
+
+for FILE in "${FILES[@]}"; do
+    if [[ -e $FILE ]]; then
+        sudo sh -c "echo 1 > $FILE"
+    fi
+done
--- intent_scripts/turn_backlight_on.sh
+++ intent_scripts/turn_backlight_on.sh
@@ -1,1 +1,11 @@
-sudo sh -c "echo 0 > /sys/class/backlight/rpi_backlight/bl_power"
+#!/bin/bash
+FILES=(
+    '/sys/class/backlight/rpi_backlight/bl_power'
+    '/sys/class/backlight/soc:backlight/bl_power'
+)
+
+for FILE in "${FILES[@]}"; do
+    if [[ -e $FILE ]]; then
+        sudo sh -c "echo 0 > $FILE"
+    fi
+done
