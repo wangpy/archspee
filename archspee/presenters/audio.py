97855f903106fba567ffda8cdc25b061cd8bdf5e
Brian Wang <wangpy.tw@gmail.com>
2019-01-28 08:00:31 -0800

    Added optional external mute script (to send IR to mute TV using external script)

--- archspee/presenters/audio.py
+++ archspee/presenters/audio.py
@@ -1,11 +1,14 @@
 from archspee.presenters import PresenterBase
 from archspee.listeners import ListenerStatus
 import alsaaudio
+from gi.repository import GLib
+import subprocess
 
 _LOG_LEVEL = None
 
+
 class AudioPresenter(PresenterBase):
-    def __init__(self, action_callback, **kwargs):
+    def __init__(self, action_callback, toggle_mute_script=None):
         self.__log_level = _LOG_LEVEL
         super(AudioPresenter, self).__init__(action_callback)
         self.status = ListenerStatus.standby
@@ -13,6 +16,11 @@ class AudioPresenter(PresenterBase):
         self.is_silenced = False
         self.audio_mixer = None
         self.orig_volume = None
+        self.toggle_mute_script = toggle_mute_script
+
+    def maybe_execute_toggle_mute(self):
+        if self.toggle_mute_script is not None:
+            GLib.idle_add(subprocess.call, ['bash', self.toggle_mute_script])
 
     def on_listener_status(self, trigger_id, status, is_disabled):
         if status != self.status or is_disabled != self.disabled:
@@ -26,10 +34,12 @@ class AudioPresenter(PresenterBase):
                     self.logger.debug('Original volume: %d' % int(self.audio_mixer.getvolume()[0]))
                     self.orig_volume = int(self.audio_mixer.getvolume()[0])
                     self.audio_mixer.setvolume(int(self.orig_volume / 10))
+                    self.maybe_execute_toggle_mute()
                     self.logger.debug('New volume: %d' % int(self.audio_mixer.getvolume()[0]))
                 else:
                     self.logger.debug('Original volume: %d' % int(self.audio_mixer.getvolume()[0]))
                     self.audio_mixer.setvolume(self.orig_volume)
+                    self.maybe_execute_toggle_mute()
                     self.logger.debug('New volume: %d' % int(self.audio_mixer.getvolume()[0]))
                     self.audio_mixer = None
                     self.orig_volume = None
@@ -53,6 +63,7 @@ class AudioPresenter(PresenterBase):
         if self.audio_mixer is not None:
             self.logger.debug('Original volume: %d' % int(self.audio_mixer.getvolume()[0]))
             self.audio_mixer.setvolume(self.orig_volume)
+            self.maybe_execute_toggle_mute()
             self.logger.debug('New volume: %d' % int(self.audio_mixer.getvolume()[0]))
             self.audio_mixer = None
             self.orig_volume = None
